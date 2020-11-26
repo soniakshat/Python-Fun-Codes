7e476e614d24f36fcf7c1518c7d3da4ef698776a
Akshat Soni <soniakshat22@gmail.com>
2020-11-26 22:46:41 +0530

    Fun Python Codes

---
+++ ASCII Art.py
@@ -0,0 +1,5 @@
+# install pywhatkit using pip install pywhatkit
+import pywhatkit as pwk
+path_to_image = "" # Example path "C:/Users/user_name/Downloads/1.jpg"
+output_file = "" # Example path "C:/Users/user_name/Downloads/1.txt"
+pwk.image_to_ascii_art(path_to_image,output_file)
