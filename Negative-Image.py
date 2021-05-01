3f4e29c89b1ac0d6de0aff67bae16b7573c0fd0d
Akshat Soni <55124419+soniakshat@users.noreply.github.com>
2021-05-01 01:22:29 -0700

    Add files via upload
    Added code that can create a negative image of the provided image.

---
+++ Negative-Image.py
@@ -0,0 +1,56 @@
+#----- Example Python program for negative transformation of a Digital Image -----
+
+ 
+
+# import Pillow modules
+
+from PIL import Image
+
+from PIL import ImageFilter
+
+ 
+
+# Load the image
+
+img = Image.open("D:/1.jpg");
+
+ 
+
+# Display the original image
+
+img.show()
+
+ 
+
+# Read pixels and apply negative transformation
+
+for i in range(0, img.size[0]-1):
+
+    for j in range(0, img.size[1]-1):
+
+        # Get pixel value at (x,y) position of the image
+
+        pixelColorVals = img.getpixel((i,j));
+
+       
+
+        # Invert color
+
+        redPixel    = 255 - pixelColorVals[0]; # Negate red pixel
+
+        greenPixel  = 255 - pixelColorVals[1]; # Negate green pixel
+
+        bluePixel   = 255 - pixelColorVals[2]; # Negate blue pixel
+
+                   
+
+        # Modify the image with the inverted pixel values
+
+        img.putpixel((i,j),(redPixel, greenPixel, bluePixel));
+
+ 
+
+# Display the negative image
+
+img.show();
+img.save("D:/5.jpg")
